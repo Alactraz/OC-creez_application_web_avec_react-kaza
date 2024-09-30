029597a0b6b8f720ec25a94d80d7940f1b9ca4fa
Alactraz <120412318+Alactraz@users.noreply.github.com>
2024-09-30 08:45:17 +0200

    correction de fautes d'orthographes

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -2,7 +2,7 @@ function Error() {
   return (
     <main className="error">
       <h1>404</h1>
-      <p>Oups! La page que vous demandez n'hexiste pas.</p>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
       <a href="/">Retournez sur la page d'accueil</a>
     </main>
   )
